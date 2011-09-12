0a28b50ec76b4c2c6d87a3c139e9168b5d219b95
Buck Doyle <b@chromatin.ca>
2011-09-12 17:01:57 -0500

    Add Codabar support

---
+++ lib/barby/barcode/codabar.rb
@@ -0,0 +1,58 @@
+require 'barby/barcode'
+
+module Barby
+
+
+  #Codabar barcodes are used in many libraries.
+  class Codabar < Barcode1D
+
+    ENCODINGS = {
+      "0" => "10101000110",
+      "1" => "10101100010",
+      "2" => "10100010110",
+      "3" => "11000101010",
+      "4" => "10110100010",
+      "5" => "11010100010",
+      "6" => "10001010110",
+      "7" => "10001011010",
+      "8" => "10001101010",
+      "9" => "11010001010",
+      "-" => "10100011010",
+      "$" => "10110001010",
+      ":" => "11010110110",
+      "/" => "11011010110",
+      "." => "11011011010",
+      "+" => "10110110110",
+      "A" => "1011000100010",
+      "B" => "1000100010110",
+      "C" => "1010001000110",
+      "D" => "1010001100010"
+    }
+
+    START = ENCODINGS["A"]
+    STOP = ENCODINGS["A"]
+
+    attr_accessor :data
+
+    def initialize(data)
+      self.data = data
+    end
+
+
+    def characters
+      data.split(//)
+    end
+
+    def encoding
+      start_encoding + characters.map{|c| ENCODINGS[c]}.join("") + stop_encoding
+    end
+
+    def start_encoding
+      START
+    end
+
+    def stop_encoding
+      STOP
+    end
+  end
+end
